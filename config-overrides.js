cf70ff8f58cc0c0ca1debe826ceb548aca310e90
tatnik <ptn48@yandex.ru>
2025-05-26 23:34:25 +0300

    set up alias support in webpack

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,29 @@
 // eslint-disable-next-line no-undef
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = function override(config /*, env*/) {
-  return {
-    ...config,
-    module: {
-      ...config.module,
-      rules: [
-        ...config.module.rules.map((rule) => {
-          if (rule.oneOf) {
-            return {
-              ...rule,
-              oneOf: [
-                {
-                  test: /icons\/.*\.svg$/,
-                  loader: 'svg-inline-loader',
-                },
-                ...rule.oneOf,
-              ],
-            };
-          }
-          return rule;
-        }),
-      ],
-    },
-  };
+    // Поддержка алиасов из tsconfig.json
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(new TsconfigPathsPlugin());
+
+ // Кастомизация svg-loader'а
+  config.module.rules = [
+    ...config.module.rules.map((rule) => {
+      if (rule.oneOf) {
+        return {
+          ...rule,
+          oneOf: [
+            {
+              test: /icons\/.*\.svg$/,
+              loader: 'svg-inline-loader',
+            },
+            ...rule.oneOf,
+          ],
+        };
+      }
+      return rule;
+    }),
+  ];
+
+  return config;
 };
