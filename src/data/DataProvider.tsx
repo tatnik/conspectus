d630a9ed1715a61eea602a3bfd7cce2542b77e6a
tatnik <ptn48@yandex.ru>
2025-06-14 10:02:55 +0300

    refactor: add Loader and error handling inside DataProvider, simplify IndexPage usage

--- src/data/DataProvider.tsx
+++ src/data/DataProvider.tsx
@@ -1,24 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { apiGetFile } from './Api';
 import { NOT_FOUND } from 'src/constants';
+import { Loader } from '@gravity-ui/uikit';
 
 interface TypeDataProviderProps {
   fileName: string;
-  renderContent: React.FC;
+  renderContent: (data: string) => React.ReactNode;
 }
 
 export const DataProvider = (props: TypeDataProviderProps) => {
   const { fileName, renderContent } = props;
   const [data, setData] = useState('');
-
-  const getPost = async () => {
-    const res = await apiGetFile(fileName);
-    setData(res.err === '' ? res.text : NOT_FOUND);
-  };
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    getPost();
+    let isMounted = true;
+    setIsLoading(true);
+    setError(null);
+
+    apiGetFile(fileName)
+      .then((res) => {
+        if (!isMounted) return;
+        if (res.err === '') {
+          setData(res.text);
+        } else {
+          setError(res.err || NOT_FOUND);
+        }
+      })
+      .catch((e) => {
+        if (isMounted) setError(String(e));
+      })
+      .finally(() => {
+        if (isMounted) setIsLoading(false);
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [fileName]);
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: 'center', padding: 24 }}>
+        <Loader size="l" />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div style={{ color: 'red', textAlign: 'center', padding: 24 }}>{error}</div>;
+  }
 
   return <>{renderContent(data)}</>;
 };
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,50 +1,56 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useAppContext } from 'src/app/AppContext/AppContextProvider';
+import { DataProvider } from 'src/data/DataProvider';
+import { apiGetImgName, apiGetNavFromIndex, apiGetNavItemByPath } from 'src/data/Api';
 
 import { NavList } from 'src/components/UI/NavList/NavList';
+import { Card, Link as LinkGravity } from '@gravity-ui/uikit';
+import { NotFound } from 'src/pages/NotFound/NotFound';
 
 import cls from './IndexPage.module.scss';
-
-import { apiGetImgName, apiGetNavFromIndex, apiGetNavItemByPath } from 'src/data/Api';
-import { DataProvider } from 'src/data/DataProvider';
-import { Card, Link as LinkGravity } from '@gravity-ui/uikit';
-import { useAppContext } from 'src/app/AppContext/AppContextProvider';
-import { useParams } from 'react-router-dom';
-import { NotFound } from '../NotFound/NotFound';
 
 export const IndexPage = () => {
   const { setCurrentPart, setShowPartNav, siteNav } = useAppContext();
   const { path } = useParams();
 
-  const navItem = apiGetNavItemByPath(path as string, siteNav);
+  const navItem = useMemo(() => apiGetNavItemByPath(path as string, siteNav), [path, siteNav]);
 
   if (navItem.id === 0 && path !== '' && path !== undefined) return <NotFound />;
 
-  const fileName = navItem.id === 0 ? '/index.md' : navItem.path + '/index.md';
+  const fileName = useMemo(
+    () => (navItem.id === 0 ? '/index.md' : navItem.path + '/index.md'),
+    [navItem]
+  );
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     setCurrentPart(navItem);
     setShowPartNav(false);
   }, [navItem]);
+
+  const renderNavList = (data: string) => (
+    <NavList
+      navLinkArray={apiGetNavFromIndex(data)}
+      classNameList={cls.nav}
+      classNameItem={cls.navItem}
+      renderProps={(val) => (
+        <Card className={cls.card}>
+          <img
+            src={apiGetImgName(val.path)}
+            loading="lazy"
+          />
+          <LinkGravity>{val.name}</LinkGravity>
+        </Card>
+      )}
+    />
+  );
 
   return (
     <div className={cls.IndexPage}>
       <DataProvider
         fileName={fileName}
-        renderContent={(data) => (
-          <NavList
-            navLinkArray={apiGetNavFromIndex(data as string)}
-            classNameList={cls.nav}
-            classNameItem={cls.navItem}
-            renderProps={function (val) {
-              return (
-                <Card className={cls.card}>
-                  <img src={apiGetImgName(val.path)} />
-                  <LinkGravity>{val.name}</LinkGravity>
-                </Card>
-              );
-            }}
-          />
-        )}
+        renderContent={renderNavList}
       />
     </div>
   );
