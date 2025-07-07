5d1b73258d5f210c5226430366b30c541355d806
tatnik <ptn48@yandex.ru>
2025-07-07 20:22:18 +0300

    add Search component

---
+++ src/components/UI/Search/Search.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { TextInput as Input, Text, Button } from '@gravity-ui/uikit';
+import { TypeNavLink } from 'src/types/nav';
+import { NavPopup } from 'src/components/UI/NavPopup/NavPopup';
+import cls from './Search.module.scss';
+
+type SearchItem = {
+  link: string; // путь вида ts/ts_oop#id
+  text: string; // заголовок
+  level: number; // уровень #, ##, ...
+};
+
+export const Search: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [index, setIndex] = useState<SearchItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [searchList, setSearchList] = useState<TypeNavLink[]>([]);
+
+  // Загрузка индекса при монтировании
+  useEffect(() => {
+    fetch('/conspectus/search-index.json')
+      .then((res) => res.json())
+      .then((data) => setIndex(data))
+      .finally(() => setLoading(false));
+  }, []);
+
+  // Массив результатов поиска
+  const results: TypeNavLink[] = useMemo(() => {
+    if (!query.trim()) return [];
+    const lower = query.toLowerCase();
+    const filterIndex = index.filter((item) => item.text.toLowerCase().includes(lower));
+    return filterIndex.map((item, id) => {
+      return {
+        id,
+        name: item.text,
+        path: item.link,
+      };
+    });
+  }, [query, index]);
+
+  let resultContent: React.ReactNode;
+  if (loading) {
+    resultContent = <Text>Загрузка...</Text>;
+  } else if (results.length === 0) {
+    resultContent = query ? <Text>не&nbsp;найдено</Text> : null;
+  } else {
+    resultContent = (
+      <NavPopup
+        navLinks={searchList}
+        handleOnClick={() => {}}
+        placement="bottom-end"
+      ></NavPopup>
+    );
+  }
+
+  return (
+    <div className={cls.Search}>
+      <Input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        size="m"
+        hasClear={true}
+        label={'Поиск:'}
+        autoFocus
+      />
+      <Button
+        aria-haspopup="menu"
+        onClick={() => setSearchList(results)}
+      >
+        &#8981;
+      </Button>
+      {resultContent}
+    </div>
+  );
+};
+
+export default Search;
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,11 +4,13 @@ import cls from './Header.module.scss';
 
 import { ThemeSwitcher } from 'src/shared/AppTheme';
 import { Nav } from 'src/components/layout/Nav/Nav';
+import Search from 'src/components/UI/Search/Search';
 
 export const Header = () => {
   return (
     <header className={cls.Header}>
       <Nav />
+      <Search />
       <ThemeSwitcher />
     </header>
   );
