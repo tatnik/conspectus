690e293d893bd200158ded4038970734a29fed2e
tatnik <ptn48@yandex.ru>
2025-07-20 19:43:40 +0300

    feat (test-utils):  add mockContext

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -4,24 +4,37 @@ import { MemoryRouter, MemoryRouterProps } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { AppThemeProvider } from './shared/AppTheme';
-import { AppContextProvider } from './app/AppContext/AppContextProvider';
+import { AppContextProvider } from './app/AppContext/AppContextProvider'; // добавь AppContext
+
 type AllProvidersProps = {
   children: ReactNode;
   route?: string;
   routerProps?: MemoryRouterProps;
+  mockContext?: any; // тип можно конкретизировать, если есть TypeAppContext
 };
 
-const AllProviders = ({ children, route = '/', routerProps }: AllProvidersProps) => (
+const AllProviders = ({ children, route = '/', routerProps, mockContext }: AllProvidersProps) => (
   <Provider store={store}>
     <AppThemeProvider>
-      <AppContextProvider>
-        <MemoryRouter
-          initialEntries={[route]}
-          {...routerProps}
-        >
-          {children}
-        </MemoryRouter>
-      </AppContextProvider>
+      {mockContext ? (
+        <AppContextProvider value={mockContext}>
+          <MemoryRouter
+            initialEntries={[route]}
+            {...routerProps}
+          >
+            {children}
+          </MemoryRouter>
+        </AppContextProvider>
+      ) : (
+        <AppContextProvider>
+          <MemoryRouter
+            initialEntries={[route]}
+            {...routerProps}
+          >
+            {children}
+          </MemoryRouter>
+        </AppContextProvider>
+      )}
     </AppThemeProvider>
   </Provider>
 );
@@ -31,6 +44,7 @@ const customRender = (
   options?: Omit<RenderOptions, 'wrapper'> & {
     route?: string;
     routerProps?: MemoryRouterProps;
+    mockContext?: any; // добавь сюда
   }
 ) =>
   render(ui, {
@@ -38,6 +52,7 @@ const customRender = (
       <AllProviders
         route={options?.route}
         routerProps={options?.routerProps}
+        mockContext={options?.mockContext}
       >
         {children}
       </AllProviders>
